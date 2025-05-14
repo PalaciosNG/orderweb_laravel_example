fcd10d86180f4f2bfdb8a3ec060418efc76240d5
PalaciosNG <nicolaspalaciosgomez@gmail.com>
2025-05-14 17:06:33 -0500

    Creación del proyecto
    Se creó el proyecto, se conectó a la base de datos y se hicieron algunos ajustes

---
+++ orderweb/app/Models/TypeActivity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TypeActivity extends Model
+{
+    use HasFactory;
+
+    protected $table = 'type_activity';
+    protected $fillable = [
+        'description',
+    ];
+}
